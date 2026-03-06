516a20d2a469470cf18c5ed9b158b1cd112357e7
sdupouy <sdupouy@zaclys.net>
2026-03-07 00:47:31 +0100

    feat: official selenium keymap

--- qmk/keymap/keymap.c
+++ qmk/keymap/keymap.c
@@ -1,124 +1,121 @@
 #include QMK_KEYBOARD_H
 #include "arsenik.h"
 
+// Shortcuts adapted for Ergol host layout
+// (In Ergol, some letters are at different physical positions than QWERTY)
+#define SC_UNDO  C(KC_Z)
+#define SC_CUT   C(KC_X)
+#define SC_COPY  C(KC_W)     // C is at W position in Ergol
+#define SC_PASTE C(KC_V)
+#define SC_REDO  C(KC_P)     // Y is at P position in Ergol
+#define SC_CLOSE C(KC_T)     // W is at T position in Ergol
+#define SC_SAVE  C(KC_S)
+#define SC_ALL   C(KC_A)
+
 enum arsenik_layers {
     _base,
-    _lafayette,
-    _num_layer,
-    _nav_layer,
-    _media_layer,
-    _mouse_layer,
-    _editor_layer,
+    _num_lock,
+    _symbols,
+    _vim_nav,
+    _num_nav,
+    _num_row,
+    _function,
 };
 
 enum custom_keycodes {
     ODK_1 = SAFE_RANGE,  // „
-    ODK_2,  // “
-    ODK_3,  // ”
+    ODK_2,  // "
+    ODK_3,  // "
     ODK_4,  // ¢
     ODK_5,  // ‰
 };
 
-// The ARSENIK_LAYOUT macro allows us to declare a config for a 4x6+3 keyboard, then truncate it
-// (or fill it with noops) depending on the size of your keyboard. Your keyboard may have extra
-// definitions for this macro or none at all (preventing you from compiling the keymap). Check
-// the `README.md` file for more information.
+// Converted from the Selenium ZMK keymap (zmk-keyboard-quacken).
 //
-// A comprehensive list of QMK keycodes is available here: https://docs.qmk.fm/keycodes
-// However, we used a many aliases to automatically adapt the keymap depending on the options you
-// enabled in the `config.h` file (or just to have some syntaxic sugar). You can find all of them
-// in the `arsenik.h` file. Feel free to remove those aliases and replace them with their actual
-// value if you need something Arsenik doesn’t provide.
+// Default thumb config (no VIM_NAVIGATION):
+//   Left:  OSM(Shift) | LT(NumNav, Bksp) | LT(Function, Tab)
+//   Right: LT(Function, Enter) | LT(NumNav, Space) | OSL(Symbols)
+//
+// Layers accessible by default: Base, NumLock, Symbols, NumNav, Function.
+// VimNav and NumRow exist but are only reachable if you change the thumb keys.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+    // 0. Base layer
     [_base] = ARSENIK_LAYOUT(
         __, __, __, __, __, __, __, __, __, __, __, __,
-        C(AS(K)),     KC_Q, KC_W,  KC_E,  KC_R,  KC_T,      KC_Y, KC_U,  KC_I,    KC_O,   KC_P,    KC_MEDIA_PLAY_PAUSE,
-        KC_ESC,       KC_A, KC_SS, KC_DD, KC_FF, KC_G,      KC_H, KC_JJ, KC_KK,   KC_LL,  KC_SCLN, KC_KB_VOLUME_UP,
-        C(AS(SLSH)),  KC_Z, KC_X,  KC_C,  KC_V,  KC_B,      KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_KB_VOLUME_DOWN,
-              AS_TL_TUCK,  AS_TL_HOME,  AS_TL_REACH,        AS_TR_REACH,  AS_TR_HOME,  AS_TR_TUCK
+        KC_TAB,   KC_Q, KC_W,  KC_E,  KC_R,  KC_T,      KC_Y, KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC,
+        KC_ESC,   KC_A, KC_SS, KC_DD, KC_FF, KC_G,      KC_H, KC_JJ, KC_KK,   KC_LL,  KC_SCLN, KC_ENT,
+        KC_LSFT,  KC_Z, KC_X,  KC_C,  KC_V,  KC_B,      KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+              OSM(MOD_LSFT),  LT(_num_nav, KC_BSPC),  LT(_function, KC_TAB),
+              LT(_function, KC_ENT),  LT(_num_nav, KC_SPC),  OSL(_symbols)
     ),
 
-    [_lafayette] = ARSENIK_LAYOUT(
+    // 1. NumLock layer -- sticky NumNav that stays on until deactivated
+    [_num_lock] = ARSENIK_LAYOUT(
         __, __, __, __, __, __, __, __, __, __, __, __,
-        __,             AS(CIRC), AS(LABK), AS(RABK), AS(DLR),  AS(PERC),      AS(AT),   AS(AMPR), AS(ASTR), AS(QUOT), AS(GRV),  CW_TOGG,
-        __,             AS(LCBR), AS(LPRN), AS(RPRN), AS(RCBR), AS(EQL),       AS(BSLS), AS(PLUS), AS(MINS), AS(SLSH), AS(DQUO), KC_CAPS,
-        QK_LAYER_LOCK,  AS(TILD), AS(LBRC), AS(RBRC), AS(UNDS), AS(HASH),      AS(PIPE), AS(EXLM), AS(SCLN), AS(COLN), AS(QUES), __,
-                                        MO(_num_layer),   KC_SPC,   __,        __,   KC_SPC,   MO(_num_layer)
+        __,  KC_ESC,   KC_HOME,  KC_UP,    KC_END,   KC_PGUP,      TO(_base), AS(7),    AS(8),    AS(9),    AS(SLSH), __,
+        __,  AS(EQL),  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,      AS(MINS),  AS(4),    AS(5),    AS(6),    AS(0),    __,
+        __,  AS(DLR),  AS(COLN), AS(ASTR), AS(PLUS), AS(PERC),     AS(COMM),  AS(1),    AS(2),    AS(3),    AS(DOT),  __,
+              __,  LT(_num_nav, KC_BSPC),  KC_TAB,
+              __,  LT(_num_nav, KC_SPC),  OSL(_symbols)
     ),
 
-    [_num_layer] = ARSENIK_LAYOUT(
+    // 2. Symbols layer -- programming symbols (AltGr layer for Ergol)
+    [_symbols] = ARSENIK_LAYOUT(
         __, __, __, __, __, __, __, __, __, __, __, __,
-        __,             AS_S1,  AS_S2,  AS_S3,  AS_S4,    AS_S5,           AS_S6,    AS_S7,     AS_S8,     AS_S9,     AS_S0,     __,
-        __,             AS(1),  AS(2),  AS(3),  AS(4),    AS(5),           AS(6),    AS(7),     AS(8),     AS(9),     AS(0),     __,
-        QK_LAYER_LOCK,  __,     __,     __,     AS(UNDS), AS(COMM),        AS(DOT),  AS(MINS),  AS(PLUS),  AS(ASTR),  AS(SLSH),  __,
-                                              __,   KC_SPC,   __,          __,   KC_SPC,   LAFAYETTE
+        __,  AS(CIRC), AS(LABK), AS(RABK), AS(DLR),  AS(PERC),     AS(AT),   AS(AMPR), AS(ASTR), AS(QUOT), AS(GRV),  __,
+        __,  AS(LCBR), AS(LPRN), AS(RPRN), AS(RCBR), AS(EQL),      AS(BSLS), AS(PLUS), AS(MINS), AS(SLSH), AS(DQUO), __,
+        __,  AS(TILD), AS(LBRC), AS(RBRC), AS(UNDS), AS(HASH),     AS(PIPE), AS(EXLM), AS(SCLN), AS(COLN), AS(QUES), __,
+              MO(_num_nav),  KC_SPC,  KC_ENT,
+              __,  KC_RALT,  __
     ),
 
-    [_nav_layer] = ARSENIK_LAYOUT(
+    // 3. VimNav layer -- HJKL arrow cluster + GUI shortcuts (not accessible by default)
+    [_vim_nav] = ARSENIK_LAYOUT(
         __, __, __, __, __, __, __, __, __, __, __, __,
-        TG(_mouse_layer),  KC_TAB,    KC_HOME,  KC_UP,    KC_END,    KC_PGUP,          KC_BRIGHTNESS_UP,    KC_F1,  KC_F2,   KC_F3,   KC_F4,   __,
-        __,                C(AS(A)),  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_PGDN,          KC_BRIGHTNESS_DOWN,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   __,
-        QK_LAYER_LOCK,     KC_UNDO,   KC_CUT,   KC_COPY,  KC_PASTE,  G(S(KC_Q)),       XX,                  KC_F9,  KC_F10,  KC_F11,  KC_F12,  __,
-                              KC_DEL,  MO(_mouse_layer),  MO(_editor_layer),           MO(_editor_layer),  MO(_mouse_layer),  KC_ESC
+        __,  KC_CAPS,  SC_CLOSE,     A(KC_LEFT),  A(KC_RGHT),  XX,         KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DEL, __,
+        __,  SC_ALL,    SC_SAVE,      S(KC_TAB),   KC_TAB,      XX,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XX,     __,
+        __,  SC_UNDO,   SC_CUT,       SC_COPY,      SC_PASTE,     SC_REDO,     XX,      XX,      XX,      XX,      XX,     __,
+              KC_CAPS,  LT(_function, KC_DEL),  MO(_num_row),
+              __,  MO(_function),  KC_RALT
     ),
 
-    [_editor_layer] = ARSENIK_LAYOUT(
+    // 4. NumNav layer -- inverted T navigation + numpad
+    [_num_nav] = ARSENIK_LAYOUT(
         __, __, __, __, __, __, __, __, __, __, __, __,
-        KC_SLEP,       KC_MENU,   XX,          KC_F2,     KC_WWW_SEARCH,  KC_WWW_REFRESH,      XX,  XX,                  XX,  XX,                  XX,  __,
-        KC_WAKE,       KC_SELECT, KC_WWW_BACK, C(AS(D)),  KC_WWW_FORWARD, KC_FIND,             XX,  KC_MEDIA_PREV_TRACK, XX,  KC_MEDIA_NEXT_TRACK, XX,  __,
-        QK_LAYER_LOCK, XX,        S(KC_LEFT),  XX,        S(KC_RGHT),     XX,                  XX,  XX,                  XX,  KC_PRINT_SCREEN,     XX,  __,
-                                                                    __,  KC_SPC,   __,         __,   KC_SPC,   __
+        __,  KC_ESC,  KC_HOME, KC_UP,   KC_END,   KC_PGUP,      TO(_num_lock), AS(7),   AS(8),   AS(9),   AS(SLSH), __,
+        __,  SC_ALL,   KC_LEFT, KC_DOWN, KC_RGHT,  KC_PGDN,      AS(MINS),      AS(4),   AS(5),   AS(6),   AS(0),    __,
+        __,  SC_UNDO,  SC_CUT,   SC_COPY,  SC_PASTE,  SC_REDO,       AS(COMM),      AS(1),   AS(2),   AS(3),   AS(DOT),  __,
+              KC_CAPS,  LT(_function, KC_DEL),  S(KC_TAB),
+              KC_ESC,  LT(_function, KC_SPC),  KC_RALT
     ),
 
-    [_media_layer] = ARSENIK_LAYOUT(
+    // 5. NumRow layer -- numbers on homerow (not accessible by default)
+    [_num_row] = ARSENIK_LAYOUT(
         __, __, __, __, __, __, __, __, __, __, __, __,
-        __,             KC_BRIGHTNESS_UP,    XX,  XX,  KC_MEDIA_PREV_TRACK,  KC_MEDIA_NEXT_TRACK,     XX, XX,  XX,  XX, KC_PRINT_SCREEN, __,
-        __,             KC_BRIGHTNESS_DOWN,  XX,  XX,  S(KC_LEFT),           S(KC_RGHT),              XX, XX,  XX,  XX, XX,              __,
-        QK_LAYER_LOCK,  XX,                  XX,  XX,  S(KC_LEFT),           S(KC_RGHT),              XX, XX,  XX,  XX, XX,              __,
-                                                            LAFAYETTE,  KC_SPC,   __,                 __,   KC_SPC,   LAFAYETTE
+        __,  AS_S1,  AS_S2,  AS_S3,  AS_S4,  AS_S5,       AS_S6,    AS_S7,    AS_S8,    AS_S9,    AS_S0,    __,
+        __,  AS(1),  AS(2),  AS(3),  AS(4),  AS(5),       AS(6),    AS(7),    AS(8),    AS(9),    AS(0),    __,
+        __,  XX,     XX,     XX,     XX,     XX,          AS(MINS), AS(COMM), AS(DOT),  AS(COLN), AS(SLSH), __,
+              __,  S(KC_SPC),  __,
+              __,  S(KC_SPC),  KC_RALT
     ),
 
-    [_mouse_layer] = ARSENIK_LAYOUT(
+    // 6. Function layer -- F1..12 + media controls + modifiers on right homerow
+    [_function] = ARSENIK_LAYOUT(
         __, __, __, __, __, __, __, __, __, __, __, __,
-        __,             MS_BTN3,  MS_BTN1,  MS_UP,    MS_BTN2,  MS_BTN4,    XX, XX, XX, XX, XX, __,
-        __,             MS_BTN5,  MS_LEFT,  MS_DOWN,  MS_RGHT,  MS_BTN6,    XX, XX, XX, XX, XX, __,
-        QK_LAYER_LOCK,  KC_WH_L,  KC_WH_U,  KC_WH_D,  KC_WH_R,  XX,         XX, XX, XX, XX, XX, __,
-                                                __,  __,  __,          __,  __,  __
+        __,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  XX,       XX,  KC_MPRV,         KC_VOLU,         KC_BRIU,  KC_SCRL, __,
+        __,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  XX,       XX,  LALT_T(KC_MPLY), RCTL_T(KC_MUTE), KC_RGUI,  KC_PSCR, __,
+        __,  KC_F9,  KC_F10, KC_F11, KC_F12, XX,       XX,  KC_MNXT,         KC_VOLD,         KC_BRID,  KC_INS,  __,
+              __,  __,  QK_BOOT,
+              QK_RBT,  __,  __
     ),
 };
 
-// This is where you’ll write most of your custom code for your keyborad.
-// This callback is called right before the keycode is sent to the OS.
-//
-// returning false cancels any furnther processing.
-// for instance, calling `tap_code(KC_B)` if KC_A is pressed but true is
-// returned, "ba" is sent, but if `false` is returned, it’s just "b"
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-#   ifdef SELENIUM_RESTORE_SPACE
-    static bool thumb_mod_same_hand_as_space_held = false;
-    if ((keycode & 0xff) == KC_SPC && record->tap.count == 0)
-        thumb_mod_same_hand_as_space_held = record->event.pressed;
-#   endif
-
     // Let QMK do its thing on key releases.
     if (!record->event.pressed) return true;
 
-#   ifdef SELENIUM_RESTORE_SPACE
-    if ((keycode & 0xff) == KC_BSPC &&
-        !thumb_mod_same_hand_as_space_held &&
-        record->tap.count > 0
-    ) {
-        tap_code(KC_SPC);
-        return false;
-    }
-#   endif
-
     switch (keycode) {
-        // ----------------------------------------
-        // Code for your custom keycodes goes here.
-        // ----------------------------------------
-
         case ODK_1: ODK1_SEQUENCE; return false;
         case ODK_2: ODK2_SEQUENCE; return false;
         case ODK_3: ODK3_SEQUENCE; return false;
@@ -130,7 +127,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 }
 
 static inline bool tap_keycode_used_in_text(uint16_t keycode) {
-    // We can’t make assumptions on curstom keycodes
+    // We can't make assumptions on custom keycodes
     if (keycode >= SAFE_RANGE) return false;
 
     // Remove "quantum" part of the keycode to get the action on tap.
